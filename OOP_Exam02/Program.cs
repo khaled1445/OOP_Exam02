335c2e56ecbd989496b26d16d58852f078a5a13a
5aled998 <khaledamr142005@.com>
2026-09-18 11:20:51 +0300

    Add project files.

---
+++ OOP_Exam02/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OOP_Exam02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
